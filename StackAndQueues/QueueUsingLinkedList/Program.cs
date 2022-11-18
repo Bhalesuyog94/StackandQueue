277e6151443a81ac65a5e780e25574ec599bda41
Bhalesuyog94 <113917613+Bhalesuyog94@users.noreply.github.com>
2022-11-19 02:31:11 +0530

    Add files via upload

---
+++ StackAndQueues/QueueUsingLinkedList/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QueueUsingLinkedList;
+
+Console.WriteLine("Welcome to Queue");
+Queue linkedListQueue = new Queue();
+linkedListQueue.Enqueue(56);
+linkedListQueue.Enqueue(30);
+linkedListQueue.Enqueue(70);
+linkedListQueue.Dequeu();
+linkedListQueue.Display();
+Console.ReadKey();
---
+++ StackAndQueues/StackAndQueues/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackUsingLinkedList;
+
+Console.WriteLine("Welcome to Stack");
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
+//linkedListStack.Peek();
+//linkedListStack.Pop();
+linkedListStack.IsEmpty();
+linkedListStack.Display();
+Console.ReadKey();
